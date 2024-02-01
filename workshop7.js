6f730b1ac6601307281cd434eb00046b04cde851
DiegoClavijo300 <munnirclavijo@gmail.com>
2024-01-31 19:50:45 -0600

    new commit

---
+++ workshop7.js
@@ -0,0 +1,16 @@
+// Create string for the message to our user 
+// Create strings for vault conbination
+// string #1 = addition 
+// string #2 = multiplication
+// string #3 = substraction
+// Create alert for user to see in website
+// Add Comments to code
+
+// String w/ the message
+const x = "You have received this message because you have been chosen to open an important vault. Here is the secret combination:";
+// strings for code conbination using adition, substrjaction and multiplication
+const str1 = 5+5 
+const str2 = 20*2
+const str3 = 40-1
+// alert for user with message and combination  
+alert(x + ' ' + str1 + " " + str2 + " " + str3)
